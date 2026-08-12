af837119c9ef1b1ff0057b193e7deae3f071ab90
Andy Davis <andy@stowzilla.com>
2026-08-12 08:48:33 -0400

    Fix Ruby 3.4 keyword argument mismatch in test stubs
    Ruby 3.4 treats _text as a distinct keyword name from text.
    The ensure block stub used _text: and _tags: but persist_overrides
    calls detect_cli_provider(text:), causing ArgumentError on 3.4.

--- test/test_discord.rb
+++ test/test_discord.rb
@@ -133,7 +133,7 @@ class TestDiscordPersistOverrides < Minitest::Test
     Brainiac::Plugins::Discord::Config.save_thread_map(map)
 
     # Temporarily make detect_cli_provider work for real
-    Object.define_method(:detect_cli_provider) { |text: "", _tags: []| (m = text.match(/\[cli:(\w+)\]/i)) ? m[1].downcase : nil }
+    Object.define_method(:detect_cli_provider) { |text: "", tags: []| (m = text.match(/\[cli:(\w+)\]/i)) ? m[1].downcase : nil }
 
     Brainiac::Plugins::Discord::Message.send(
       :persist_overrides, "galen:thread123", "[cli:kiro] hello", nil,
@@ -144,7 +144,7 @@ class TestDiscordPersistOverrides < Minitest::Test
     updated = Brainiac::Plugins::Discord::Config.load_thread_map
     assert_equal "kiro", updated["galen:thread123"]["cli_provider"]
   ensure
-    Object.define_method(:detect_cli_provider) { |_text: "", _tags: []| nil }
+    Object.define_method(:detect_cli_provider) { |text: "", tags: []| nil }
   end
 
   def test_persist_overrides_updates_thread_map_model
